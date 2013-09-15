e3aaba5a1b720ecf001bf0c99340abd54254e0d6
Jacob Schlitz <jschlitz@gmail.com>
2013-09-15 14:51:10 -0400

    Added the console app that generates dir.js

---
+++ SlideShow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace SlideShow
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+      var items = Directory.GetFiles(dir, "*.jpg").ToList();
+      items.AddRange(Directory.GetFiles(dir, "*.jpeg"));
+      items.AddRange(Directory.GetFiles(dir, "*.gif"));
+      items.AddRange(Directory.GetFiles(dir, "*.png"));
+
+      using (var sw = new StreamWriter(Path.Combine(dir, "dir.js"), false))
+      {
+        sw.WriteLine("var IMAGES = [");
+        foreach (var item in items)
+          sw.WriteLine("  \"{0}\",", Path.GetFileName(item)); //seems we don't have to worry about the extra comma
+        sw.WriteLine("];");
+      }
+    }
+  }
+}
